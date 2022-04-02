f74e99110e53cc9d22605ffb3528cd7301aa8ae0
Hufnagels <kbvconsulting@gmail.com>
2022-04-02 23:14:28 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  useQuery,
+} from "@apollo/client";
 
-function App() {
+// Material
+import {
+  Avatar,
+  Box,
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+} from '@mui/material'
+import { red } from '@mui/material/colors';
+import IconButton from '@mui/material/IconButton';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+// Custom
+import { GET_USERS, GET_MAPS } from "./app/queries";
+
+const App = () => {
+  const getAllUsers = useQuery(GET_USERS);
+  const getAllMaps = useQuery(GET_MAPS);
+
+  if (getAllUsers.loading ) return "Loading";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Box style={{padding:'0.5rem'}}>
+        <Grid container spacing={{ xs: 2, md: 3 }} >
+          <GridItem fn={getAllUsers.data} title="Users"/>
+          <GridItem fn={getAllMaps.data} title="Maps"/>
+        </Grid>
+      </Box>
+    </React.Fragment>
   );
 }
 
 export default App;
+
+const GridItem = ({fn, title}) => {
+  return (
+    <Grid item xs={12} sm={12} md={6} lg={4} xl={3}>
+            <Card variant="outlined" >
+              <CardHeader
+                avatar={
+                  <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">R</Avatar>
+                }
+                action={
+                  <IconButton aria-label="settings">
+                    <MoreVertIcon />
+                  </IconButton>
+                }
+                title={title}
+                subheader="September 14, 2016"
+              />
+              <CardContent>
+                <pre>{JSON.stringify(fn, null, 2)}</pre>
+              </CardContent>
+            </Card>
+          </Grid>
+  )
+}
